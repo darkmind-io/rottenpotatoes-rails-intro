6cca95803597e20e919f2cdef1018067a012e42c
darkmind-io <jclark17@citadel.edu>
2018-10-01 03:56:08 +0000

    part 3 progress

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -21,7 +21,7 @@ class MoviesController < ApplicationController
     if params[:ratings] != session[:@selected_ratings] && !params[:ratings].nil? then 
       session[:@selected_ratings] = params[:ratings] 
     elsif session[:@selected_ratings].nil? then
-      session[:@selected_ratings] = @all_ratings
+      session[:@selected_ratings] = {}
     end
     
     case session[:sort_state]
